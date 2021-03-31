5e771d932c28bc6dcc69d50982be1c05c744c18d
Mathias Latronico <mathiezelat@gmail.com>
2021-03-31 17:02:25 -0300

    xShop - v0.0.17

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -19,6 +19,15 @@ const NavBar = () =>{
         setActiveDropdown(!activeDropdown)
         onClickUp()
     }
+    const ClickUpSlideOff = ()=>{
+        onClickUp()
+        setActiveSlide(false)
+        setActiveSlide(false)
+    }
+    const DropSlide = ()=>{
+        setActiveSlide(false)
+        onClickUp()
+    }
 
     return (
         <header>
@@ -27,18 +36,18 @@ const NavBar = () =>{
                     <NavLink to='/' onClick={onClickUp}><img src="/images/logo.svg" alt="xShop Logo"/></NavLink>
                 </div>
                 <ul className={`nav-links ${activeSlide ? 'nav-active nav-active-mov' : ''}`}>
-                    <li><NavLink to='/' exact activeClassName="active"  onClick={onClickUp}  >Inicio</NavLink></li>
+                    <li><NavLink to='/' exact activeClassName="active"  onClick={ClickUpSlideOff}  >Inicio</NavLink></li>
                     <li className="sub-nav"><NavLink to='#' activeClassName="no" onMouseDown={Dropdown}>Categorias
                     <svg className="arrow-expand" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M24 24H0V0h24v24z" fill="none" opacity="0"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
                     </NavLink>
                     <ul className={`sub-nav-links ${activeDropdown ? 'active-subnav' : ''}`}>
-                        <li><NavLink to={`/category/iMac`} onClick={Dropdown}>iMac</NavLink></li>
-                        <li><NavLink to={`/category/MacBook`} onClick={Dropdown}>MacBook</NavLink></li>
-                        <li><NavLink to={`/category/iPhone`} onClick={Dropdown}>iPhone</NavLink></li>
-                        <li><NavLink to={`/category/Watch`} onClick={Dropdown}>Watch</NavLink></li>
+                        <li><NavLink to={`/category/iMac`} onClick={DropSlide}>iMac</NavLink></li>
+                        <li><NavLink to={`/category/MacBook`} onClick={DropSlide}>MacBook</NavLink></li>
+                        <li><NavLink to={`/category/iPhone`} onClick={DropSlide}>iPhone</NavLink></li>
+                        <li><NavLink to={`/category/Watch`} onClick={DropSlide}>Watch</NavLink></li>
                     </ul>
                     </li>
-                    <li><NavLink to='/nosotros' activeClassName="active">Nosotros</NavLink></li>
+                    <li><NavLink to='/nosotros' activeClassName="active" >Nosotros</NavLink></li>
                     <li><NavLink to='/ayuda' activeClassName="active" >Ayuda</NavLink></li>
                     <CartWidget />
                 </ul>
