0460b6058ffbc1102053bb297d313d9046a6c858
Mathias Latronico <mathiezelat@gmail.com>
2021-03-31 17:10:41 -0300

    xShop - v0.0.18

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './NavBar.scss';
 import CartWidget from '../CartWidget/index';
 import {animateScroll as scroll} from 'react-scroll'
@@ -17,18 +17,20 @@ const NavBar = () =>{
     }
     const Dropdown = ()=>{
         setActiveDropdown(!activeDropdown)
-        onClickUp()
     }
     const ClickUpSlideOff = ()=>{
         onClickUp()
         setActiveSlide(false)
-        setActiveSlide(false)
+        setActiveDropdown(false)
     }
     const DropSlide = ()=>{
         setActiveSlide(false)
+        setActiveDropdown(false)
         onClickUp()
     }
-
+    useEffect(()=>{
+        
+    }, [])
     return (
         <header>
             <nav>
@@ -36,6 +38,7 @@ const NavBar = () =>{
                     <NavLink to='/' onClick={onClickUp}><img src="/images/logo.svg" alt="xShop Logo"/></NavLink>
                 </div>
                 <ul className={`nav-links ${activeSlide ? 'nav-active nav-active-mov' : ''}`}>
+                    {activeDropdown ? <div className="dropdown-off" onMouseEnter={Dropdown} ></div> : null }
                     <li><NavLink to='/' exact activeClassName="active"  onClick={ClickUpSlideOff}  >Inicio</NavLink></li>
                     <li className="sub-nav"><NavLink to='#' activeClassName="no" onMouseDown={Dropdown}>Categorias
                     <svg className="arrow-expand" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ffffff"><path d="M24 24H0V0h24v24z" fill="none" opacity="0"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>
