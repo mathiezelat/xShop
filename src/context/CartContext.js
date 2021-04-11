1c2df11fa353c92100e635bd8c99cd30448a857a
Mathias Latronico <mathiezelat@gmail.com>
2021-04-11 17:31:33 -0300

    xShop - v0.0.28

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,10 +1,14 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const CartContext = createContext({});
 
 export const CartProviderContext = ({children}) =>{
     const [cart, setCart] = useState([])
-
+    useEffect(()=>{
+        if (localStorage.getItem("cart") !==null){
+            setCart(JSON.parse(localStorage.getItem("cart")));
+        }
+    }, [])
     const CartLength = () =>{
         return cart.reduce((prev, next) => {return prev + next.quantity}, 0)
     }
@@ -15,18 +19,22 @@ export const CartProviderContext = ({children}) =>{
     const addItem = (newItem, newQuantity)=>{
         if (isInCart(newItem.id) === -1){
             setCart([...cart, { item: newItem, quantity: newQuantity }])
+            localStorage.setItem("cart", JSON.stringify([...cart, { item: newItem, quantity: newQuantity }]))
         } else {
             const {quantity} = cart.find(e => e.item.id === newItem.id)
             const newCart = cart.filter(e => e.item.id !== newItem.id)
             setCart([...newCart, { item: newItem , quantity: quantity + newQuantity }])
+            localStorage.setItem("cart", JSON.stringify([...newCart, { item: newItem , quantity: quantity + newQuantity }]))
         }
     }
     const removeItem = (itemId) =>{
         const newCart = cart.filter(e=>e.item.id !== itemId)
         setCart(newCart)
+        localStorage.setItem("cart", JSON.stringify(newCart))
     }
     const clear = ()=>{
         setCart([])
+        localStorage.setItem("cart", JSON.stringify([]))
     }
     const isInCart = (id) =>{
         return cart.findIndex(e=> e.item.id === id)
